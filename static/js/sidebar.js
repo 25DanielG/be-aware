4a5edf4f6201fcec1dab2f162abbc08ea358d02b
Daniel Gergov <danalmaden24@gmail.com>
2025-09-27 10:47:42 -0400

    fix sidebar animation

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,6 +1,10 @@
 const logo = document.querySelector('aside .sidebar .logo');
 const sidebar = document.querySelector('aside');
 
+setTimeout(() => {
+    document.body.classList.add('initialized');
+}, 300);
+
 if (logo) {
     logo.addEventListener("click", () => {
         sidebar.classList.toggle("closed");
